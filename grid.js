b2b95a9c87cc64262ce7bff954c67794c5f6d6da
Damani Philip <38143975+d-philip@users.noreply.github.com>
2019-03-23 11:44:47 -0400

    Added homepage and P5 JS source

---
+++ grid.js
@@ -0,0 +1,107 @@
+var x,y;
+//Matrix for randomizing initial states of the stoplight at each intersection
+var stopLightStatus =[[Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4)],
+                      [Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4)],
+                      [Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4)],
+                      [Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4)],
+                      [Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4),Math.floor(Math.random()*4)]];
+var counter;
+//Number of seconds to wait before switching times fps
+const seconds = 5*60;
+let car;
+
+function setup() {
+  createCanvas(1040,1040);
+  counter = seconds;
+  //Randomly generates initial states for each stop light
+  frameRate(60);
+  car = new Car();
+}
+
+function draw() {
+  //Draws background and buildings
+  background(80);
+  fill(255);
+  for(var i = 0; i < 6; i++){
+    for(var j = 0; j < 6; j++){
+      rect(170*i+30 ,170*j+30, 120,120);
+    }
+  }
+
+  //Draws cars and moves them
+  car.move();
+  car.brake();
+  car.display();
+
+  //Draws stoplights
+  for(var i = 0; i < 5; i++){
+    for(var j = 0; j < 5; j++){
+      //Draws Top light (0)
+      fill(chooseColor(i,j,0) ? 0 : 255,chooseColor(i,j,0) ? 255 : 0,0);
+      rect(150 + 170*i,140+ 170 * j,52,10);
+      //Draws Right light (1)
+      fill(chooseColor(i,j,1) ? 0 : 255,chooseColor(i,j,1) ? 255 : 0,0);
+      rect(200 + 170 * i,150 + 170*j,10,52);
+      //Draws Bottom light (2)
+      fill(chooseColor(i,j,2) ? 0 : 255,chooseColor(i,j,2) ? 255 : 0,0);
+      rect(150 + 170*i,200+ 170 * j,52,10);
+      //Draws Left light (3)
+      fill(chooseColor(i,j,3) ? 0 : 255,chooseColor(i,j,3) ? 255 : 0,0);
+      rect(140 + 170 * i,150 + 170*j,10,52);
+    }
+  }
+
+  counter++;
+  //Changes each stoplight after seconds amount of time
+  if(counter >= seconds){
+    for(var i = 0; i < 5; i++){
+      for(var j = 0; j < 5; j++){
+        if(stopLightStatus[i][j] == 3){
+          stopLightStatus[i][j] = 0;
+        }
+        else{
+          stopLightStatus[i][j] = stopLightStatus[i][j] + 1;
+        }
+      }
+    }
+    counter = 0;
+  }
+
+}
+
+//Decides whether he light should be green or red
+function chooseColor(i,j,pos){
+  if(stopLightStatus[i][j] == pos){
+      return true;
+  }
+  else {
+      return false;
+  }
+}
+
+class Car{
+  constructor(){
+    this.x = 50;
+    this.y = 180;
+    this.speed = 1;
+    this.c = color(100, 150, 200);
+  }
+  move(){
+    this.x += 1;
+    //this.y += random(-this.speed, this.speed);
+  }
+  brake(){
+    if(this.speed > 0)
+    {
+      this.speed -= 0.3
+    }
+  }
+  display(){
+    fill(this.c);
+    rect(this.x, this.y, 10, 10);
+  }
+}
+
+class Ambulance{
+
+}
